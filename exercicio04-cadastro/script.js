731a04ee6904bb29701cc1d87f4d3c0c0250c9dc
Maria Vicente <mnvcaetano@gmail.com>
2023-06-10 22:15:57 -0300

    ex04 cadastro de aluno

---
+++ exercicio04-cadastro/script.js
@@ -0,0 +1,9 @@
+var aluno = {
+    nome: "",
+    idade: 0
+}
+
+aluno.nome = prompt("Digite o nome do aluno:");
+aluno.idade = prompt("Digite a idade do aluno:");
+
+alert("O(A) aluno(a) " + aluno.nome + " tem " + aluno.idade + " anos.");
